cd217a09df4a8c2a7af0758c62275cb354375682
Viper <Viper@Viper-NW>
2020-11-06 17:47:54 +0200

    First demo exercise

--- src/ajlab_1/AJLab.java
+++ src/ajlab_1/AJLab.java
@@ -5,12 +5,26 @@
  */
 package ajlab_1;
 
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import static javafx.scene.paint.Color.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -18,23 +32,52 @@ import javafx.stage.Stage;
  * @author Viper
  */
 public class AJLab extends Application {
+
+    public Text createtext(){
+        Text t=new Text(10,20,"Advanced Java");
+        t.setFont(Font.font("Arial",FontPosture.ITALIC,20));
+        t.setUnderline(true);
+        t.setFill(BLUE);
+        return t;
+    }
+    
+    ImageView createpath() throws FileNotFoundException{
+        InputStream in=new FileInputStream("D:\\skull.png");
+        Image image=new Image(in,400,400,true,true);
+        ImageView image1=new ImageView(image);
+        return image1;
+    }
+    
+    ImageView createIV(){
+        Image image=new Image("https://homepages.cae.wisc.edu/~ece533/images/fruits",200,200,true,true);
+        ImageView image1=new ImageView(image);
+        
+        return image1;
+    }
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage) throws FileNotFoundException {
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//            
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//        
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+        VBox vbox=new VBox(createtext(),createIV(),createpath());
+//        Pane pane=new Pane();
+//        pane.setPadding(new Insets(5));
+//        
+//        pane.getChildren().add(createtext());
+//        pane.getChildren().add(createIV());
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(vbox, 300, 250);
+        scene.setFill(AQUA);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
